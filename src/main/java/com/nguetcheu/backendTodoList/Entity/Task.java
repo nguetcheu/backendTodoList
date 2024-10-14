ba724a968ff7fa4b1b65b0b5084c1c41336bd43c
nguetcheu <dnguetcheu@gmail.com>
2024-10-14 21:25:59 +0200

    Mise en place de la géneration de la date de facon automatique

--- src/main/java/com/nguetcheu/backendTodoList/Entity/Task.java
+++ src/main/java/com/nguetcheu/backendTodoList/Entity/Task.java
@@ -3,11 +3,13 @@ package com.nguetcheu.backendTodoList.Entity;
 import jakarta.persistence.*;
 import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.Date;
 
 @Entity
 @Data
+@EntityListeners(AuditingEntityListener.class)  // Important pour activer l'audit
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
